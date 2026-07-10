ad37fb51db606d8d4727a868db5b8ec99629d1b6
Mahir Puri <mpuri@uvic.ca>
2026-07-10 10:16:50 -0700

    feat: add the production env

---
+++ config/environments/production.rb
@@ -0,0 +1,14 @@
+require "active_support/core_ext/integer/time"
+
+Rails.application.configure do
+  config.enable_reloading = false
+  config.eager_load = true
+  config.consider_all_requests_local = false
+
+  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
+  config.log_tags = [:request_id]
+  config.logger = ActiveSupport::TaggedLogging.new(Logger.new($stdout))
+
+  config.active_record.dump_schema_after_migration = false
+  config.force_ssl = ENV["FORCE_SSL"] == "1"
+end
